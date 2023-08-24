806e7fcaf867a1a390c05e66a7e1585b6eca2626
Aria Balaei <ariabalayi@gmail.com>
2023-08-24 20:14:15 +0200

    several updates

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const canvas = d3.select('.canva');
 
 var data = [
-    45.00,78.25,89.26,75.26,42.75,62.69,54.59,36.03,82.01,25.00,41.00,65.00,76.00,98.26,105.00,12.98,25.65,36.00,42.65,25.33,88.63,145.00,355.00,122.15,547.00,54.25,488.22
+    45,56,
+    78,79,
+    73,75,
+    75,98,
+    102,142,
+    224,256,
 ];
 
 
-var months = ['January','February','March','April','May','June','July','August','September','October','November ','December'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
 
 var parseMonths = d3.timeParse('%B');
@@ -24,13 +29,22 @@ const mainCanvas = svg.append('g')
                 .attr('width', graphWidth)
                 .attr('transform',`translate(${margin.left},${margin.top})`);
 
+//set range and domain
+var x = d3.scaleTime()
+                .domain(d3.extent(months, d => parseMonths(d)))
+                .range([0, graphWidth])
+
+var y = d3.scaleLinear()
+            .domain([0, d3.max(data, (d) => d)])
+            .range([graphHeight, 0])
+
 var areaChart= d3.area()
-                 .x((d, i) => i*40)
+                 .x((d, i) => {return x(parseMonths(months[i]))})
                  .y0(graphHeight)
-                 .y1((d, i) => (graphHeight - d))      
-                 
+                 .y1((d, i) => (graphHeight - d))   
+
 mainCanvas.append('path')
-            .attr('fill' ,'orange')
+            .attr('fill' ,'green')
             .attr('class', 'area')
             .attr('d', areaChart(data))
 
